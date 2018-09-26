2fd8142eaff81346de3d225864330452c17e3925
CatsAreMLG <joshuadgon@gmail.com>
2018-09-26 18:59:53 -0500

    push to heroku

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  require 'httparty'
+
+  def index
+    response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=4726206&units=imperial&appid=' + Figaro.env.weather_api_key)
+    @json = JSON.parse(response.body)
+    @temperature = @json["main"]["temp"]
+    @windSpeed = @json["wind"]["speed"]
+    @windDirection = @json["wind"]["deg"]
+  end
+end
