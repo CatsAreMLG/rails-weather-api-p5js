93f67c3f6f55b77d5db4224dab527b1021dbfdee
CatsAreMLG <joshuadgon@gmail.com>
2018-09-26 19:56:42 -0500

    fixed string bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   require 'httparty'
 
   def index
-    response = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=4726206&units=imperial&appid=' + Figaro.env.weather_api_key)
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?id=4726206&units=imperial&appid=#{Figaro.env.weather_api_key}")
     @json = JSON.parse(response.body)
     @temperature = @json["main"]["temp"]
     @windSpeed = @json["wind"]["speed"]
