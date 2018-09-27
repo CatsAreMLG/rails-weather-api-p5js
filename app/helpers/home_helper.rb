8004a96f15f5ce463b36e93f42c987f4faba8de6
CatsAreMLG <joshuadgon@gmail.com>
2018-09-26 23:34:39 -0500

    Added get location

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+    def location(lat, lon)
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{lat},&lon=#{lon}&units=imperial&appid=#{Figaro.env.weather_api_key}")
+        @json = JSON.parse(response.body)
+        @temperature = @json["main"]["temp"]
+        @windSpeed = @json["wind"]["speed"]
+        @windDirection = @json["wind"]["deg"]
+    end
 end
